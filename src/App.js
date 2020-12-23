4c0bce26fc64c67859a9af12d144b941a8293979
mycogno <jdg4661@gmail.com>
2020-12-23 23:59:35 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css";
 import HookUseState from "./useState";
 import HookUseInput from "./useInput";
+import HookUseTabs from "./useTabs";
 
 function App() {
   return (
     <div>
       <HookUseState initNumber={0} />
       <HookUseInput />;
+      <HookUseTabs />;
     </div>
   );
 }
